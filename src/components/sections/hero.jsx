a16f7c3edf6ebc85bae984b177cf5f22e029b50d
Jaiane <jaianesoares700@gmail.com>
2026-06-01 00:33:27 -0300

    feat: ajustado imagens não renderizadas da home

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,21 +1,28 @@
+import React from 'react';
 import './hero.css';
-import linhaBranca from '../../assets/icons/linhaBranca.svg';
-import imgHero from '../../assets/images/home/imgSection1.png';
 import { Link } from 'react-router-dom';
 
-const Hero = () =>{
-    return(
+// Importamos apenas a linha branca, que é usada como uma tag <img> no conteúdo
+import linhaBranca from '../../assets/icons/linhaBranca.svg';
+
+const Hero = () => {
+    return (
+        // A classe "hero" vai puxar a imagem de fundo automaticamente pelo CSS
         <section className="hero">
             <div className="titulo_hero">
                 <h1>Sua voz, sem barreiras</h1>
-                <img src={linhaBranca}/>
+                
+                {/* Linha decorativa abaixo do título */}
+                <img src={linhaBranca} alt="Linha Decorativa" />
+                
                 <p>IA e design para transformar seu sopro em voz</p>
-                <Link to="/produto" style={{ textDecoration: 'none'}}>
+                
+                <Link to="/produto" style={{ textDecoration: 'none' }}>
                     <button className="saibaMaisHero">SAIBA MAIS</button>
                 </Link>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
