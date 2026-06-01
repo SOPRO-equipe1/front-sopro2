2a91a104a4acc20c8dbd0179a60316f1fed5ea82
Bruno Souza <brunoasouza15004@gmail.com>
2026-06-01 16:28:30 -0300

    ajustando botão hero

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -4,19 +4,17 @@ import { Link } from 'react-router-dom';
 import linhaBranca from '../../assets/icons/linhaBranca.svg';
 import imgHero from '../../assets/images/home/imgSection1.png';
 
-const Hero = () =>{
-    return(
+const Hero = () => {
+    return (
         <section className="hero" style={{ backgroundImage: `url(${imgHero})` }}>
             <div className="titulo_hero">
                 <h1>Sua voz, sem barreiras</h1>
-                
-                {/* Linha decorativa abaixo do título */}
                 <img src={linhaBranca} alt="Linha Decorativa" />
-                
                 <p>IA e design para transformar seu sopro em voz</p>
                 
-                <Link to="/produto" style={{ textDecoration: 'none' }}>
-                    <button className="saibaMaisHero">SAIBA MAIS</button>
+                {/* Usamos o próprio Link como botão para evitar conflitos de renderização */}
+                <Link to="/produto" className="saibaMaisHero">
+                    SAIBA MAIS
                 </Link>
             </div>
         </section>
