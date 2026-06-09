c91372306f47e3656c36a0da79a05ea6e6c00476
Rubens Silva <rubensfrs_@hotmail.com>
2026-06-09 02:45:42 -0300

    corrigindo footer

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -11,56 +11,43 @@ const Footer = () => {
       <div className="footer-wrapper">
         <div className="footer-columns">
           <div className="footer-col">
-            <div className="footer-logo-brand">
-              <img src={soproLogo} alt="SOPRO Logo" />
-
-              <span>SOPRO</span>
-            </div>
+            <img className="logo-sopro" src={soproLogo} alt="SOPRO Logo" />
             <p className="footer-desc">
               Redefinindo comunicação através de <br />
-              tecnologia assistiva de próxima <br />
-              geração.
+              tecnologia assistiva de próxima geração.
             </p>
           </div>
 
           <div className="footer-col">
-            <h4>Produto</h4>
+            <h4>Navegação</h4>
             <ul className="footer-list">
-              <li><a href="/produto">Recursos</a></li>
-              <li><a href="/home">Especificações</a></li>
-              <li><a href="/planos">Preços</a></li>
-              <li><a href="/suporte">Suporte</a></li>
+              <li><a href="/">Início</a></li>
+              <li><a href="/sobrenos">Sobre Nós</a></li>
+              <li><a href="/produto">Produto</a></li>
             </ul>
           </div>
 
           <div className="footer-col">
-            <h4>Empresa</h4>
+            <h4>Área do Usuário</h4>
             <ul className="footer-list">
-              <li><a href="/sobre">Sobre Nós</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Carreiras</a></li>
+              <li><a href="/planos">Preços</a></li>
               <li><a href="/suporte">Contato</a></li>
+              <li><a href="/perfil">Conta</a></li>
             </ul>
           </div>
 
           <div className="footer-col">
             <h4>Conecte-se</h4>
             <div className="footer-social-icons">
-              <a href="#" className="social-icon-box"><img src={linkedin} alt="Linkedin" /></a>
               <a href="https://www.instagram.com/sopro.voz/" className="social-icon-box"> <img src={instagramIcon} alt="Instagram" /></a>
-              <a href="#" className="social-icon-box"><img src={youtubeIcon} alt="YoutubeFooter" /></a>
+              <a href="https://www.youtube.com/@Sopro-voz" className="social-icon-box"><img src={youtubeIcon} alt="YoutubeFooter" /></a>
               <a href="https://github.com/SOPRO-equipe1" className="social-icon-box"><img src={githubIcon} alt="GitHubFooter" /></a>
             </div>
           </div>
         </div>
 
         <div className="footer-bottom">
-          <p className="footer-copy">© 2026 Sopro AI. Todos os direitos reservados.</p>
-          <div className="footer-legal">
-            <a href="#">Privacidade</a>
-            <a href="#">Termos</a>
-            <a href="#">Cookies</a>
-          </div>
+          <p className="footer-copy">© 2026 Sopro. Todos os direitos reservados.</p>
         </div>
       </div>
     </footer>
